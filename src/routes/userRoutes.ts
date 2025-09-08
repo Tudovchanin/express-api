3ddaaa96cacaf3b1d556a62cad4d1ce6bf303cf9
Vlad Halimov <sotto36623@gmail.com>
2025-09-08 18:10:38 +0300

    refactor(user-routes): change block/unblock routes from POST to PATCH

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -9,10 +9,10 @@ router.get('/',  userController.getUsers);
 router.get('/me', userController.getCurrentUser);
 router.get('/:id', userController.getUserById);
 
-router.post('/block/me', userController.blockCurrentUser);
-router.post('/block/:id', userController.blockUser);
+router.patch('/block/me', userController.blockCurrentUser);
+router.patch('/block/:id', userController.blockUser);
 
-router.post('/unblock/me', userController.unblockCurrentUser);
-router.post('/unblock/:id', userController.unblockUser);
+router.patch('/unblock/me', userController.unblockCurrentUser);
+router.patch('/unblock/:id', userController.unblockUser);
 
 export default router;
